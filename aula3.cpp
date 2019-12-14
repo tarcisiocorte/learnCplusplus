00cafdbc2e6f90481e8fe6a329a4b797ddf17483
tarcisiocorte <tarcisiocorte@gmail.com>
2019-12-14 13:21:57 -0300

    primeiro código - aula 3

---
+++ aula3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+int main(){
+	
+	setlocale(LC_ALL, "Portuguese");
+
+	cout << "Fazendo um pequeno teste no C++\nTarcísio Costa Corte" << endl;
+	system("pause");
+	return 0;
+}
+
