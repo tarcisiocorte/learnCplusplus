637401d19214a51bbe9b6c79a7a136f4b46982a2
tarcisiocorte <tarcisiocorte@gmail.com>
2019-12-14 14:36:32 -0300

    Curso de C++ #04 - Variáveis em C++

---
+++ aula4.cpp
@@ -0,0 +1,17 @@
+//Curso de C++ #04 - Variáveis em C++
+#include <iostream>
+using namespace std;
+int main(){	
+	
+	int vidas = 0;
+	char letra = 'C';	
+	double decimal = 5.2; // 2.4999999
+	float decimal2 = 5.2; // 2.5
+	bool vivo = true; // true or false
+	string nome = "Tarcísio Costa Corte de Paula";
+	
+	cout << nome;
+
+	return 0;
+}
+
