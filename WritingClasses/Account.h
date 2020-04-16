3bec04b806753b6d67d9bcdb27c7cddf21fcc002
Tarcisio Corte <tarcisiocorte@gmail.com>
2020-04-16 16:50:01 -0300

    exemplo de uso de classes em C++

---
+++ WritingClasses/Account.h
@@ -0,0 +1,18 @@
+#include <vector>
+#include <string>
+#include "Transaction.h"
+
+class Account
+{
+private:
+	int balance;
+	std::vector<Transaction> log;
+	int limit;
+public:
+	Account();
+	std::vector<std::string> Report();
+	bool Deposit(int amt);
+	bool Withdraw(int amt);
+	int GetBalance(){return balance;}
+};
+
